4b129289f4752a85fd8b46943c9f27f0620c38a7
Paul Done <pkdone@gmail.com>
2023-02-04 16:50:46 +0000

    improved init repset code

--- mongod/mongod-runextra.sh
+++ mongod/mongod-runextra.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Wait until local MongoDB instance is up and running
-until /usr/bin/mongosh --port 27017 --quiet --eval 'db.getMongo()'; do
-    sleep 1
-done
-
-# Configure a MongoDB replica set if this replica has been asked to
 if [ "$DO_INIT_REPSET" = true ] ; then
+    # Wait until local MongoDB instance is up and running
+    until /usr/bin/mongosh --port 27017 --quiet --eval 'db.getMongo()'; do
+        sleep 1
+    done
+
+    # Configure a MongoDB replica set if this replica has been asked to
     /usr/bin/mongosh --port 27017 <<EOF
         rs.initiate({_id: "${REPSET_NAME}", members: [
             {_id: 0, host: "${REPSET_NAME}-replica0:27017"},
